d1492fd45ae7380a0ea27b4829ea0cf5ef8c72a7
huntercobra18 <caillotantoine2023@gmail.com>
2022-01-25 16:59:39 +0100

    test

---
+++ command.php
@@ -0,0 +1,42 @@
+<?php 
+header("Location: html/index.php");
+
+function bdd_connection(string $user="root",string $pass=""){
+    try {
+        $dbh = new PDO('mysql:host=localhost;dbname=fcems', $user, $pass);
+    } catch (PDOException $e) {
+        echo "<script language=javascript>
+        console.log('erreur " . $e->getMessage() ."');
+    </script>";
+        die();
+    }
+    echo "<script language=javascript>
+        console.log('Page bien connectée à la base de données');
+    </script>";
+    return $dbh;
+}
+
+// voir si pas usine a gaz
+function get_rank(string $rank="entraineur"){
+    $nb=0;
+    switch ($rank) {
+        case 'admin':
+            $nb===4;
+            break;
+        case 'president':
+        case 'conseil administration':
+            $nb===3;
+            break;
+        case 'staff':
+            $nb===2;
+            break;
+        case 'entraineur':
+            $nb===1;
+            break;
+        default:
+            $nb===0;
+            break;
+    }
+    return $nb;
+}
+?>
---
+++ html/connect.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+include "../command.php";
+$bdd=bdd_connection();
+
+if(isset($_POST["user"])){
+    if(isset($_POST["pass"])){
+        $pass=hash("sha256",$_POST["pass"]);
+        if($rep=$bdd->query("select name from staff where name = ".$_POST["user"]." and password = $pass")){
+            if(!empty($rep)){
+                $_SESSION["connection"]=true;
+                $_SESSION["token"]["name"]=$rep["name"];
+                $_SESSION["token"]["pass"]=$pass;
+                header("Location: ../private/index.php");
+            } else {
+                echo '<div id="error">Votre identifiant / mot de passe est incorrect</div>';
+            }
+        }
+    } else {
+        // manque le mdp (gerer l erreur)
+    }
+} else {
+    // manque le nom dutilisateur (gerer l erreur)
+    unset($_POST["pass"]);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connection</title>
+</head>
+<body>
+    <form action="" method="post">
+        <p>
+            <label for="user">User</label>
+            <input type='text' name='user' id ='user' size='25' pattern='[A-Za-z0-9]{3-20}' required autofocus>
+        </p>
+        <p>
+            <label for="pass">Password</label>
+            <input type='text' name='pass' id ='pass' size='25' required>
+        </p>
+    </form>
+</body>
+</html>
