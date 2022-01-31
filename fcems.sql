3a2c3b88e08b5940dcee08facd44d8530f84486f
huntercobra18 <caillotantoine2023@gmail.com>
2022-01-31 21:55:10 +0100

    ajout type article

--- fcems.sql
+++ fcems.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 28 jan. 2022 à 01:06
+-- Généré le : lun. 31 jan. 2022 à 21:54
 -- Version du serveur :  10.4.17-MariaDB
 -- Version de PHP : 8.0.0
 
@@ -30,12 +30,20 @@ SET time_zone = "+00:00";
 CREATE TABLE `article` (
   `id-article` int(10) UNSIGNED NOT NULL,
   `titre` varchar(80) NOT NULL,
-  `keyword` varchar(200) NOT NULL,
+  `keyword` varchar(100) NOT NULL,
   `sub` varchar(500) NOT NULL,
   `texte` text NOT NULL,
   `auteur` int(10) UNSIGNED NOT NULL,
-  `date` varchar(20) NOT NULL
+  `date` varchar(20) NOT NULL,
+  `type` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `article`
+--
+
+INSERT INTO `article` (`id-article`, `titre`, `keyword`, `sub`, `texte`, `auteur`, `date`, `type`) VALUES
+(1, 'Article victoire senior', 'senior;victoire', 'Lorem ipsum dolor sit amet. Et assumenda illo ut rerum dolorem non sunt impedit ut amet expedita vel ipsam ratione! Aut voluptatem ipsam sit dolorum consequatur eos fugiat exercitationem qui modi dolorem.', 'Lorem ipsum dolor sit amet. Et assumenda illo ut rerum dolorem non sunt impedit ut amet expedita vel ipsam ratione! Aut voluptatem ipsam sit dolorum consequatur eos fugiat exercitationem qui modi dolorem. Cum omnis amet id incidunt sint est totam dignissimos ut dolores nihil est voluptatem recusandae in unde odit.\r\n\r\nEa galisum vero aut officiis enim ad deleniti voluptates nam quas itaque ad Quis incidunt. Est nihil harum quo rerum enim qui ipsam velit eum recusandae sint ab autem temporibus et temporibus amet. Et voluptas alias et omnis nemo hic ipsum rerum non provident nulla.\r\n\r\nEst rerum accusantium et perspiciatis quisquam hic iste necessitatibus est velit galisum ea nesciunt inventore in officia laborum aut dolor nisi? Eum quasi quam non aspernatur voluptas qui commodi excepturi non aspernatur maxime et tenetur veritatis eos ducimus consequatur. Eum sunt corporis est ducimus voluptate sed aliquam illum sit vero molestias et quos asperiores quo neque vitae. In quaerat sint eum deleniti saepe sed dicta neque 33 quod veritatis eos fuga nihil?', 1, '20220130', 'type1');
 
 -- --------------------------------------------------------
 
@@ -300,7 +308,7 @@ ALTER TABLE `staff`
 -- AUTO_INCREMENT pour la table `article`
 --
 ALTER TABLE `article`
-  MODIFY `id-article` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id-article` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `categorie`
