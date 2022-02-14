f85d91876bc6a379204d756898d937702de8ec77
huntercobra18 <truc@gmail.com>
2022-02-14 17:15:22 +0100

    creation page joueur

---
+++ private/joueur.php
@@ -0,0 +1,16 @@
+<?php 
+session_start();
+if(isset($_SESSION["connection"])&&($_SESSION["connection"]===true)&&(isset($_SESSION["token"]["pass"]))&&(isset($_SESSION["token"]["name"]))){
+    include "../command.php";
+    $bdd=bdd_connection();
+    $rep=$bdd->query("select name,type from staff where password = '".$_SESSION["token"]["pass"]."'")->fetch();
+    if($rep["name"]===$_SESSION["token"]["name"]){
+        $rep=$bdd->query("select * from staff where name = '".$_SESSION["token"]["name"]."'")->fetch();
+    } else {
+        session_destroy();
+        header("Location: ../html/connect.php");
+    }
+} else {
+    session_destroy();
+    header("Location: ../html/connect.php");
+}
