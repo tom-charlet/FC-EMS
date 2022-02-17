cddb4509569a38a1f2e41c86992c6ef6319f911a
huntercobra18 <truc@gmail.com>
2022-02-17 21:05:25 +0100

    ajout affichage equipe

--- private/joueur.php
+++ private/joueur.php
@@ -14,3 +14,37 @@ if(isset($_SESSION["connection"])&&($_SESSION["connection"]===true)&&(isset($_SE
     session_destroy();
     header("Location: ../html/connect.php");
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Joueur</title>
+</head>
+<body>
+    <h3>Joueur</h3>
+    <form id='for' method='post'>
+    <?php 
+    if(isset($_POST["action"])){$_SESSION["joueur"]["action"]=$_POST["action"];}
+    ?>
+    <p>
+    <button name="action" type="submit" value="add" <?php if(isset($_SESSION["joueur"]["action"])&&$_SESSION["joueur"]["action"]!=="add"){echo "class='grey'";}?>>Ajouter un joueur</button>
+    <button name="action" type="submit" value="del" <?php if(isset($_SESSION["joueur"]["action"])&&$_SESSION["joueur"]["action"]!=="del"){echo "class='grey'";}?>>Supr un joueur</button>
+    <button name="action" type="submit" value="mod" <?php if(isset($_SESSION["joueur"]["action"])&&$_SESSION["joueur"]["action"]!=="mod"){echo "class='grey'";}?>>Mod un joueur</button>
+    </p>
+    </form>
+    <?php
+    if(isset($_SESSION["joueur"]["action"])&&($_SESSION["joueur"]["action"]==="del"||$_SESSION["joueur"]["action"]=="mod")){ // sup et mod de joueur 
+        $equipe = $bdd->query("SELECT * from equipe")->fetchAll(PDO::FETCH_ASSOC);
+        $team='';
+        foreach ($equipe as $key => $value) {
+            if(isset($_SESSION["joueur"]["equipe"])&&$_SESSION["joueur"]["action"]==="add"){$check='class="select"';}else{$check='';}
+            $team.="<button name='equipe' type='submit' value='".$equipe[$key]["id_equipe"]."'>".$equipe[$key]["nom"]."</button>";
+        }
+        echo '<form method="post"><p>'.$team.'</p></form>';
+    }
+    ?>
+</body>
+</html>
