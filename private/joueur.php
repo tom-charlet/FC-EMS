e8a9033ce3d58930ad3cf1dd25895b66013d760c
huntercobra18 <caillotantoine2023@gmail.com>
2022-02-28 14:39:15 +0100

    deplacement traitement

--- private/joueur.php
+++ private/joueur.php
@@ -14,6 +14,52 @@ if(isset($_SESSION["connection"])&&($_SESSION["connection"]===true)&&(isset($_SE
     session_destroy();
     header("Location: ../html/connect.php");
 }
+
+// traitement ajout ( a tester)
+if(isset($_SESSION["joueur"]["action"])&&$_SESSION["joueur"]["action"]=="add"&&isset($_POST["nom"])){
+    if(isset($_FILES["media"])){
+        $_FILES["media"]["name"]=str_replace("|"," ",$_FILES["media"]["name"]);
+        while(file_exists("../img/".$_FILES["media"]["name"])){
+            $_FILES["media"]["name"].=1; // permet d'eviter qu'un fichier existe 2 fois
+        }
+        if (move_uploaded_file($_FILES["media"]["tmp_name"],"../img/".$_FILES["media"]["name"])){
+            $bdd->query("insert into media (nom, equipe, type) VALUES ('".$_FILES["media"]["name"]."|Photo de ".$_POST['nom']." ".$_POST['prenom']."', ".$_POST['equipe'].",'joueur'");
+            echo "image ajouté dans la bdd";
+            $joueur=$bdd->query("select * from media where nom = '".$_FILES["media"]["name"]."|Photo de ".$_POST['nom']." ".$_POST['prenom']."' ")->fetch();
+        }
+    } else {echo"Aucune image n a été ajouté";}
+    // ajout joueur bdd
+    if(isset($joueur)){
+        $bdd->query("insert into joueur (nom, prenom, equipe, photo) VALUES ('".$_POST['nom']."', '".$_POST['prenom']."',".$_POST['equipe'].",".$joueur."");
+        echo "joueur ajouté avec equipe";
+    } else {
+        $bdd->query("INSERT into joueur (nom, prenom, equipe) VALUES ('".$_POST['nom']."', '".$_POST['prenom']."',".$_POST['equipe']."");
+        echo "joueur ajouté";
+    }
+}
+
+if(isset($_POST["equipe"])){$_SESSION["joueur"]["equipe"]=$_POST["equipe"];}
+
+//traitement sup/mod
+if(isset($_POST["joueur"])){
+    //cas sup 
+    if($_SESSION["joueur"]["action"]==='del'){
+        $img=$bdd->query("select photo from joueur where id_joueur = ".$_POST["joueur"]."")->fetch();
+        var_dump($img);
+        if($img["photo"]!==NULL){
+            $supr=$bdd->query("select nom from media where id_media = ".$img["photo"]."")->fetch();
+            if(unlink("../img/".explode("|",$supr["nom"])[0])){
+                echo "image supprimer sur le serveur" ;
+            }
+            if($bdd->query("DELETE from media where id_media = ".$img["photo"]."")){
+                echo "image supprimer sur la bdd" ;
+            }
+        }
+        if($bdd->query("DELETE from joueur where id_joueur = ".$_POST["joueur"]."")->fetch()){
+            echo "joueur bien supr";
+        }
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -49,7 +95,7 @@ if(isset($_SESSION["connection"])&&($_SESSION["connection"]===true)&&(isset($_SE
         <input type="text" name="prenom" id ="prenom" maxlength="50" size="25" placeholder="Prenom" required>
         '.$option.'</select>
         <label for="titre">Photo du joueur</label>
-        <input type="file" name="media" id ="media" required class="hidden">
+        <input type="file" name="media" id ="media" class="hidden">
         <button type="submit" form="add">Valider</button>
         ';// faire formulaire
     }
@@ -87,38 +133,18 @@ if(isset($_SESSION["connection"])&&($_SESSION["connection"]===true)&&(isset($_SE
         }
         echo '<form method="post"><p>'.$team.'</p></form>';
     }
-    if(isset($_POST["equipe"])){$_SESSION["joueur"]["equipe"]=$_POST["equipe"];}
-
     
     // affichage joueur
-    if(isset($_SESSION["joueur"]["action"])&&($_SESSION["joueur"]["action"]=="del"||$_SESSION["joueur"]["action"]=="mod")){
+    if(isset($_SESSION["joueur"]["action"])&&($_SESSION["joueur"]["action"]=="del"||$_SESSION["joueur"]["action"]=="mod")&&isset($_SESSION["joueur"]["equipe"])){
         $joueur=$bdd->query("select * from joueur where equipe = ".$_SESSION["joueur"]["equipe"]."")->fetchAll();
         $player='';
         foreach ($joueur as $key => $value) {
-            $player.="<button name='joueur' type='submit' value='".$joueur[$key]["nom"]."|".$joueur[$key]["prenom"]."'>".$joueur[$key]["nom"]." ".$joueur[$key]["prenom"]."</button>";
+            $player.="<button name='joueur' type='submit' value='".$joueur[$key]["id_joueur"]."'>".$joueur[$key]["nom"]." ".$joueur[$key]["prenom"]."</button>";
         }
         echo '<form method="post">'.$player.'</form>';
     }
 
-    //traitement sup/mod
-    if(isset($_POST["joueur"])){
-        //cas sup 
-        if($_SESSION["joueur"]["action"]==='del'){
-            $img=$bdd->query("select photo from joueur where nom = '".explode("|",$_POST["joueur"])[0]."' AND prenom = '".explode("|",$_POST["joueur"])[1]."'")->fetc;
-            if($img["photo"]!==NULL){
-                $supr=$bdd->query("select nom from media where id_media = ".$img["photo"]."")->fetch();
-                if(unlink("../img/".explode("|",$supr["nom"])[0])){
-                    echo "image supprimer sur le serveur" ;
-                }
-                if($bdd->query("DELETE from media where id_media = ".$img["photo"]."")){
-                    echo "image supprimer sur la bdd" ;
-                }
-            }
-        }
-        if($bdd->query("DELETE from joueur where id_media = ".$img["photo"]."")){
-            echo "joueur bien supr";
-        }
-    }
+    
     ?>
 </body>
 </html>
