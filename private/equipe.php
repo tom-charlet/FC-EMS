279cd2907c1b4001aafbde1dc288db143eadbc61
huntercobra18 <truc@gmail.com>
2022-03-07 22:02:52 +0100

    correction affichage valeurs

--- private/equipe.php
+++ private/equipe.php
@@ -82,8 +82,8 @@ if(isset($_SESSION["equipe"]["action"])&&$_SESSION["equipe"]["action"]==='mod'){
             //point d'arret
 
             $categorie=$bdd->query("SELECT categorie.*,equipe.* from categorie INNER JOIN equipe ON categorie.equipe = equipe.id_equipe where categorie.id = ".$_POST["categorie"])->fetch();
-            $categorie["id_joueur"]='<input type="hidden" name="photo" value="'.$categorie["id_joueur"].'">';
-            $categorie["nom"]="value='".$categorie["nom"]."'";
+            $categorie["id_equipe"]='<input type="hidden" name="photo" value="'.$categorie["id_equipe"].'">';
+            $categorie["categorie"]="value='".$categorie["categorie"]."'";
             $categorie["lien"]="value='".explode("|",$categorie["lien"])[0]."'";
             $categorie["mot"]="value='".explode("|",$categorie["lien"])[1]."'";
         }
@@ -98,11 +98,11 @@ if(isset($_SESSION["equipe"]["action"])&&$_SESSION["equipe"]["action"]==='mod'){
             }
         }
         //si $categorie n'est pas defini
-        if(!isset($categorie)){$categorie["id_joueur"]="";$categorie["nom"]='';$categorie["lien"]='';$categorie["mot"]='';}
+        if(!isset($categorie)){$categorie["id_equipe"]="";$categorie["nom"]='';$categorie["lien"]='';$categorie["mot"]='';}
         echo '<form method="post" id="add" enctype="multipart/form-data">'
         //cette ligne permet le transfert de l 'id categorie pour la mod
-        .$categorie["id_joueur"].
-        '<input type="text" name="nom" id ="categorie" maxlength="30" size="25" placeholder="Categorie" '.$categorie["nom"].' required autofocus >
+        .$categorie["id_equipe"].
+        '<input type="text" name="nom" id ="categorie" maxlength="30" size="25" placeholder="Categorie" '.$categorie["categorie"].' required autofocus >
         <select name="equipe" id="equipe">'.$option.'</select>
         <input type="text" name="lien" id ="lien" maxlength="450" size="60" placeholder="Lien des matchs" '.$categorie["lien"].' required>
         <input type="text" name="mot" id ="mot" maxlength="50" size="60" placeholder="Mot de découpe" '.$categorie["mot"].' required>
