74be99c307e7ba0d44ffa9a323f304d4e90c4d87
huntercobra18 <truc@gmail.com>
2022-03-14 15:45:19 +0100

    affichage liste staff

--- private/staff.php
+++ private/staff.php
@@ -16,27 +16,27 @@ if(isset($_SESSION["connection"])&&($_SESSION["connection"]===true)&&(isset($_SE
 }
 
 // traitement ajout
-if(isset($_SESSION["equipe"]["action"])&&$_SESSION["equipe"]["action"]==="add"&&isset($_POST["nom"])){
-    // if(empty($bdd->query("SELECT * from equipe where nom = '".$_POST["nom"]."'")->fetch())){
-    // echo $bdd->query("INSERT INTO equipe (nom) VALUES ('".$_POST["nom"]."')")->fetch();
-    // echo "equipe ajoutée";
+if(isset($_SESSION["staff"]["action"])&&$_SESSION["staff"]["action"]==="add"&&isset($_POST["nom"])){
+    // if(empty($bdd->query("SELECT * from staff where nom = '".$_POST["nom"]."'")->fetch())){
+    // echo $bdd->query("INSERT INTO staff (nom) VALUES ('".$_POST["nom"]."')")->fetch();
+    // echo "staff ajoutée";
     // } else {
     //     echo "l équipe existe deja";
     // }
 }
 
 //traitement sup
-if(isset($_SESSION["equipe"]["action"])&&$_SESSION["equipe"]["action"]==='del'&&isset($_POST["id_equipe"])){
-    // $bdd->query("DELETE from equipe where id_equipe = ".$_POST["id_equipe"]);
+if(isset($_SESSION["staff"]["action"])&&$_SESSION["staff"]["action"]==='del'&&isset($_POST["id_staff"])){
+    // $bdd->query("DELETE from staff where id_staff = ".$_POST["id_staff"]);
     // echo "categorie del de la bdd";
-    // // equipe del de la bdd
+    // // staff del de la bdd
 }
 
 //traitement mod
-if(isset($_SESSION["equipe"]["action"])&&$_SESSION["equipe"]["action"]==='mod'&&isset($_POST["id"])){
-    // $bdd->query("UPDATE equipe set nom = '".$_POST["nom"]."' where id_equipe = ".$_POST["id"]."");
-    // echo "equipe update";
-    // unset($_POST["id_equipe"]);
+if(isset($_SESSION["staff"]["action"])&&$_SESSION["staff"]["action"]==='mod'&&isset($_POST["id"])){
+    // $bdd->query("UPDATE staff set nom = '".$_POST["nom"]."' where id_staff = ".$_POST["id"]."");
+    // echo "staff update";
+    // unset($_POST["id_staff"]);
 }
 
 ?>
@@ -52,41 +52,41 @@ if(isset($_SESSION["equipe"]["action"])&&$_SESSION["equipe"]["action"]==='mod'&&
     <h3>Staff</h3>
     <form id='for' method='post'>
     <?php 
-    if(isset($_POST["action"])){$_SESSION["equipe"]["action"]=$_POST["action"];}
+    if(isset($_POST["action"])){$_SESSION["staff"]["action"]=$_POST["action"];}
     ?>
     <p>
-    <button name="action" type="submit" value="add" <?php if(isset($_SESSION["equipe"]["action"])&&$_SESSION["equipe"]["action"]!=="add"){echo "class='grey'";}?>>Ajouter un staff</button>
-    <button name="action" type="submit" value="del" <?php if(isset($_SESSION["equipe"]["action"])&&$_SESSION["equipe"]["action"]!=="del"){echo "class='grey'";}?>>Supr un staff</button>
-    <button name="action" type="submit" value="mod" <?php if(isset($_SESSION["equipe"]["action"])&&$_SESSION["equipe"]["action"]!=="mod"){echo "class='grey'";}?>>Mod un staff</button>
+    <button name="action" type="submit" value="add" <?php if(isset($_SESSION["staff"]["action"])&&$_SESSION["staff"]["action"]!=="add"){echo "class='grey'";}?>>Ajouter un staff</button>
+    <button name="action" type="submit" value="del" <?php if(isset($_SESSION["staff"]["action"])&&$_SESSION["staff"]["action"]!=="del"){echo "class='grey'";}?>>Supr un staff</button>
+    <button name="action" type="submit" value="mod" <?php if(isset($_SESSION["staff"]["action"])&&$_SESSION["staff"]["action"]!=="mod"){echo "class='grey'";}?>>Mod un staff</button>
     </p>
     </form>
     <?php
 
-    // sup et mod d'equipe (affichage liste)
-    if(isset($_SESSION["equipe"]["action"])&&($_SESSION["equipe"]["action"]==="del"||$_SESSION["equipe"]["action"]=="mod")){  
-        // $equipe = $bdd->query("SELECT * from equipe")->fetchAll(PDO::FETCH_ASSOC);
-        // $equ='';
-        // foreach ($equipe as $key => $value) {
-        //     if(isset($_SESSION["equipe"]["equipe"])&&$_SESSION["equipe"]["action"]==="add"){$check='class="select"';}else{$check='';}
-        //     $equ.="<button name='id_equipe' type='submit' value='".$equipe[$key]["id_equipe"]."'>".$equipe[$key]["nom"]."</button>";
-        // }
-        // echo '<form method="post"><p>'.$equ.'</p></form>';
+    // sup et mod d'staff (affichage liste)
+    if(isset($_SESSION["staff"]["action"])&&($_SESSION["staff"]["action"]==="del"||$_SESSION["staff"]["action"]=="mod")){  
+        $staff = $bdd->query("SELECT * from staff")->fetchAll(PDO::FETCH_ASSOC);
+        $sta='';
+        foreach ($staff as $key => $value) {
+            if(isset($_SESSION["staff"]["staff"])&&$_SESSION["staff"]["action"]==="add"){$check='class="select"';}else{$check='';}
+            $sta.="<button name='id_staff' type='submit' value='".$staff[$key]["id_staff"]."'>".strtoupper($staff[$key]["nom"])." ".$staff[$key]["prenom"]."</button>";
+        }
+        echo '<form method="post"><p>'.$sta.'</p></form>';
     }
 
 
     // affichage d'ajout / mod (affichage form)
-    if((isset($_SESSION["equipe"]["action"])&&$_SESSION["equipe"]["action"]=="add") || (isset($_SESSION["equipe"]["action"])&&$_SESSION["equipe"]["action"]==='mod'&&isset($_POST["id_equipe"]))){ 
-        // if($_SESSION["equipe"]["action"]==='mod'){
-        //     $equipe=$bdd->query("SELECT * from equipe where id_equipe = ".$_POST["id_equipe"])->fetch();
-        //     $equipe["id_equipe"]='<input type="hidden" name="id" value="'.$equipe["id_equipe"].'">';
-        //     $equipe["nom"]="value='".$equipe["nom"]."'";
+    if((isset($_SESSION["staff"]["action"])&&$_SESSION["staff"]["action"]=="add") || (isset($_SESSION["staff"]["action"])&&$_SESSION["staff"]["action"]==='mod'&&isset($_POST["id_staff"]))){ 
+        // if($_SESSION["staff"]["action"]==='mod'){
+        //     $staff=$bdd->query("SELECT * from staff where id_staff = ".$_POST["id_staff"])->fetch();
+        //     $staff["id_staff"]='<input type="hidden" name="id" value="'.$staff["id_staff"].'">';
+        //     $staff["nom"]="value='".$staff["nom"]."'";
         // }
-        // // //si $equipe n'est pas defini
-        //  if(!isset($equipe)){$equipe["id_equipe"]="";$equipe["nom"]="";}
+        // // //si $staff n'est pas defini
+        //  if(!isset($staff)){$staff["id_staff"]="";$staff["nom"]="";}
         // echo '<form method="post" id="add" enctype="multipart/form-data">'
-        // //cette ligne permet le transfert de l 'id equipe pour la mod
-        // .$equipe["id_equipe"].
-        // '<input type="text" name="nom" id ="nom" maxlength="30" size="25" placeholder="Equipe" '.$equipe["nom"].' required autofocus >
+        // //cette ligne permet le transfert de l 'id staff pour la mod
+        // .$staff["id_staff"].
+        // '<input type="text" name="nom" id ="nom" maxlength="30" size="25" placeholder="staff" '.$staff["nom"].' required autofocus >
         // <button type="submit" form="add">Valider</button>
         // ';
     }
