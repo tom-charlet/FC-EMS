7733c37f5327c171ad6cbf8db6fd8f07080ae6ae
huntercobra18 <caillotantoine2023@gmail.com>
2022-03-28 09:56:25 +0200

    ajout créa fichier page

--- private/article.php
+++ private/article.php
@@ -14,103 +14,6 @@ if(isset($_SESSION["connection"])&&($_SESSION["connection"]===true)&&(isset($_SE
     session_destroy();
     header("Location: ../html/connect.php");
 }
-
-if(isset($_POST["edit"])){
-    $_SESSION["article"]=$bdd->query("SELECT * from article where id_article = ".$_POST["edit"]."")->fetch();
-    $_SESSION["article"]["action"]="edit";
-}
-
-
-if(!isset($_SESSION["article"])){
-    $_SESSION["article"]=[];
-}
-if(isset($_POST["id_article"])){$_SESSION["article"]["id_article"]=$_POST["id_article"];}
-if(isset($_POST["titre"])){$_SESSION["article"]["titre"]=$_POST["titre"];}
-if(isset($_POST["keyword"])){$_SESSION["article"]["keyword"]=$_POST["keyword"];}
-if(isset($_POST["sub"])){$_SESSION["article"]["sub"]=$_POST["sub"];}
-if(isset($_POST["texte"])){$_SESSION["article"]["texte"]=$_POST["texte"];}
-if(isset($_POST["date"])){$_SESSION["article"]["date"]=$_POST["date"];}
-if(isset($_POST["type"])){$_SESSION["article"]["type"]=$_POST["type"];}
-
-
-// traitement creation / modificaion
-if(isset($_SESSION["article"]["action"])&&isset($_POST['sub'])){
-    // cas ajout
-    if($_SESSION["article"]["action"]==="add"){
-        $tmp=$bdd->query("select * from article where titre = '".$_SESSION["article"]["titre"]."'")->fetch();
-        if(empty($tmp)){
-            if($bdd->query("INSERT INTO article (`titre`, `keyword`, `sub`, `texte`, `auteur`, `date`, `type`) VALUES ('".$_SESSION["article"]["titre"]."','".$_SESSION["article"]["keyword"]."','".$_SESSION["article"]["sub"]."','".$_SESSION["article"]["texte"]."',".$rep["id_staff"].",'".$_SESSION["article"]["date"]."','".$_SESSION["article"]["type"]."')")){
-                echo '<div id="error">L article a bien ete creer</div>';
-            }
-            // unset($_SESSION["article"]);
-        } else {
-            echo '<div id="error">Un article avec le meme nom existe deja</div>';
-        }
-    } else if($_SESSION["article"]["action"]==="mod"&&isset($_POST["id_article"])){
-        if($bdd->query("UPDATE article SET `titre` = '".$_SESSION["article"]["titre"]."', `keyword`= '".$_SESSION["article"]["keyword"]."', `sub`= '".$_SESSION["article"]["sub"]."', `auteur`= ".$rep["id_staff"].", `date`= '".$_SESSION["article"]["date"]."' , `type`= '".$_SESSION["article"]["type"]."' where id_article = ".$_POST["id_article"]."")){
-            echo "Article modifié";
-        }
-        $supr=$bdd->query("SELECT nom from media where article = ".$_POST["id_article"]."")->fetchAll(PDO::FETCH_ASSOC);
-        foreach ($supr as $key => $value) {
-            if(unlink("../img/".explode("|",$value["nom"])[0])){
-                echo "image supprimer sur le serveur" ;
-            }
-        }   
-        if($bdd->query("DELETE from media where article = ".$_POST["id_article"]."")){
-            //images supr de la bdd
-        }
-    }
-
-    //test si il y a des photos
-    if($_FILES["upload"]["size"][0]!=0){
-        //traitement des images (creer un tableau ou chaque index est une photo valide)
-        $photo=[];
-        foreach ($_FILES["upload"]["name"] as $key => $value) {
-            if($_FILES["upload"]["error"][$key]==0&&explode("/",$_FILES["upload"]["type"][$key])[0]=="image"){
-                $photo[]=["name"=>(nom().".".explode("/",$_FILES["upload"]["type"][$key])[1]),"full_path"=>$_FILES["upload"]["full_path"][$key],"type"=>$_FILES["upload"]["type"][$key],"tmp_name"=>$_FILES["upload"]["tmp_name"][$key],"error"=>$_FILES["upload"]["error"][$key],"size"=>$_FILES["upload"]["size"][$key]];
-            }
-        }
-    
-        //recuperation id pour photos
-        if($_SESSION["article"]["action"]==="mod"){
-            $id=$_POST["id_article"];
-        } else if ($_SESSION["article"]["action"]==="add"){
-            $id=$bdd->query("SELECT id_article where `date` = '".$_SESSION["article"]["date"]."' AND titre = '".$_SESSION["article"]["titre"]."'")->fetch();
-            $id=$id['id_article'];
-        }
-        
-
-        //ajout des photos
-        if(isset($id)){
-            foreach ($photo as $key => $value) {
-                while(file_exists("../img/".$value["name"])){
-                    $value["name"]=nom().".".explode("/",$_FILES["upload"]["type"][$key])[1]; // permet d'eviter qu'un fichier n existe pas 2 fois
-                }
-                if (move_uploaded_file($value["tmp_name"],"../img/".$value["name"])) {
-                    $bdd->query("insert into media (nom, type,article) VALUES ('".$value["name"]."|".($a=(isset($_POST["pic-desc"])?$_POST["pic-desc"]:"photo n ".($key+1)." de l'article du ".$_SESSION["article"]["date"]))."','".explode("/",$value["type"])[0]."',".$id.")");
-                    echo "Le média a été ajouté dans le serveur";
-                    unset($_SESSION["media"]);
-                } else {echo "<p>Probleme lors du televersement</p>";}
-            }
-        }
-    }
-}
-
-//traitement supr
-if(isset($_POST["delete"])){
-    $supr=$bdd->query("SELECT nom from media where article = ".$_POST["delete"]."")->fetchAll(PDO::FETCH_ASSOC);
-    foreach ($supr as $key => $value) {
-        if(unlink("../img/".explode("|",$value["nom"])[0])){
-            echo "image supprimer sur le serveur" ;
-        }
-    }   
-    if($bdd->query("DELETE from media where article = ".$_POST["delete"]."")){
-        //images supr de la bdd
-    }
-    if($bdd->query("DELETE from article where id_article = ".$_POST["delete"]."")){
-        //article supr de la bdd
-    }
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -121,6 +24,109 @@ if(isset($_POST["delete"])){
     <title>Document</title>
 </head>
 <body>
+    <?php 
+    if(isset($_POST["edit"])){
+        $_SESSION["article"]=$bdd->query("SELECT * from article where id_article = ".$_POST["edit"]."")->fetch();
+        $_SESSION["article"]["action"]="edit";
+    }
+    
+    
+    if(!isset($_SESSION["article"])){
+        $_SESSION["article"]=[];
+    }
+    if(isset($_POST["id_article"])){$_SESSION["article"]["id_article"]=$_POST["id_article"];}
+    if(isset($_POST["titre"])){$_SESSION["article"]["titre"]=$_POST["titre"];}
+    if(isset($_POST["keyword"])){$_SESSION["article"]["keyword"]=$_POST["keyword"];}
+    if(isset($_POST["sub"])){$_SESSION["article"]["sub"]=$_POST["sub"];}
+    if(isset($_POST["texte"])){$_SESSION["article"]["texte"]=$_POST["texte"];}
+    if(isset($_POST["date"])){$_SESSION["article"]["date"]=$_POST["date"];}
+    if(isset($_POST["type"])){$_SESSION["article"]["type"]=$_POST["type"];}
+    
+    
+    // traitement creation / modificaion
+    if(isset($_SESSION["article"]["action"])&&isset($_POST['sub'])){
+        // cas ajout
+        if($_SESSION["article"]["action"]==="add"){
+            $tmp=$bdd->query("select * from article where titre = '".$_SESSION["article"]["titre"]."'")->fetch();
+            if(empty($tmp)){
+                if($bdd->query("INSERT INTO article (`titre`, `keyword`, `sub`, `texte`, `auteur`, `date`, `type`) VALUES ('".$_SESSION["article"]["titre"]."','".$_SESSION["article"]["keyword"]."','".$_SESSION["article"]["sub"]."','".$_SESSION["article"]["texte"]."',".$rep["id_staff"].",'".$_SESSION["article"]["date"]."','".$_SESSION["article"]["type"]."')")){
+                    echo '<div id="error">L article a bien ete creer</div>';
+                }
+                // unset($_SESSION["article"]);
+            } else {
+                echo '<div id="error">Un article avec le meme nom existe deja</div>';
+            }
+            if(isset($_SESSION["article"]["type"])){
+                if(copy("../assets/pages/article_template_".$_SESSION["article"]["type"],"../article/".$_SESSION["article"]["titre"].".php")){
+                    //page créé
+                }
+            }
+        } else if($_SESSION["article"]["action"]==="mod"&&isset($_POST["id_article"])){
+            if($bdd->query("UPDATE article SET `titre` = '".$_SESSION["article"]["titre"]."', `keyword`= '".$_SESSION["article"]["keyword"]."', `sub`= '".$_SESSION["article"]["sub"]."', `auteur`= ".$rep["id_staff"].", `date`= '".$_SESSION["article"]["date"]."' , `type`= '".$_SESSION["article"]["type"]."' where id_article = ".$_POST["id_article"]."")){
+                echo "Article modifié";
+            }
+            $supr=$bdd->query("SELECT nom from media where article = ".$_POST["id_article"]."")->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($supr as $key => $value) {
+                if(unlink("../img/".explode("|",$value["nom"])[0])){
+                    echo "image supprimer sur le serveur" ;
+                }
+            }   
+            if($bdd->query("DELETE from media where article = ".$_POST["id_article"]."")){
+                //images supr de la bdd
+            }
+        }
+    
+        //test si il y a des photos
+        if($_FILES["upload"]["size"][0]!=0){
+            //traitement des images (creer un tableau ou chaque index est une photo valide)
+            $photo=[];
+            foreach ($_FILES["upload"]["name"] as $key => $value) {
+                if($_FILES["upload"]["error"][$key]==0&&explode("/",$_FILES["upload"]["type"][$key])[0]=="image"){
+                    $photo[]=["name"=>(nom().".".explode("/",$_FILES["upload"]["type"][$key])[1]),"full_path"=>$_FILES["upload"]["full_path"][$key],"type"=>$_FILES["upload"]["type"][$key],"tmp_name"=>$_FILES["upload"]["tmp_name"][$key],"error"=>$_FILES["upload"]["error"][$key],"size"=>$_FILES["upload"]["size"][$key]];
+                }
+            }
+        
+            //recuperation id pour photos
+            if($_SESSION["article"]["action"]==="mod"){
+                $id=$_POST["id_article"];
+            } else if ($_SESSION["article"]["action"]==="add"){
+                $id=$bdd->query("SELECT id_article where `date` = '".$_SESSION["article"]["date"]."' AND titre = '".$_SESSION["article"]["titre"]."'")->fetch();
+                $id=$id['id_article'];
+            }
+            
+    
+            //ajout des photos
+            if(isset($id)){
+                foreach ($photo as $key => $value) {
+                    while(file_exists("../img/".$value["name"])){
+                        $value["name"]=nom().".".explode("/",$_FILES["upload"]["type"][$key])[1]; // permet d'eviter qu'un fichier n existe pas 2 fois
+                    }
+                    if (move_uploaded_file($value["tmp_name"],"../img/".$value["name"])) {
+                        $bdd->query("insert into media (nom, type,article) VALUES ('".$value["name"]."|".($a=(isset($_POST["pic-desc"])?$_POST["pic-desc"]:"photo n ".($key+1)." de l'article du ".$_SESSION["article"]["date"]))."','".explode("/",$value["type"])[0]."',".$id.")");
+                        echo "Le média a été ajouté dans le serveur";
+                        unset($_SESSION["media"]);
+                    } else {echo "<p>Probleme lors du televersement</p>";}
+                }
+            }
+        }
+    }
+    
+    //traitement supr
+    if(isset($_POST["delete"])){
+        $supr=$bdd->query("SELECT nom from media where article = ".$_POST["delete"]."")->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($supr as $key => $value) {
+            if(unlink("../img/".explode("|",$value["nom"])[0])){
+                echo "image supprimer sur le serveur" ;
+            }
+        }   
+        if($bdd->query("DELETE from media where article = ".$_POST["delete"]."")){
+            //images supr de la bdd
+        }
+        if($bdd->query("DELETE from article where id_article = ".$_POST["delete"]."")){
+            //article supr de la bdd
+        }
+    }
+    ?>
     <table>
         <tr>
             <th><p>Titre</p></th>
@@ -175,8 +181,6 @@ if(isset($_POST["delete"])){
             </div>
             <div id="part2">
                 './*Transimssion id pour modification*/($a=(isset($_SESSION["article"]["id_article"]))?'<input type="hidden" name="id_article" value="'.$_SESSION["article"]["id_article"].'">':"").'
-                <!-- form a gerer en js (a voir)-->
-
                 <p>
                     <label for="titre">Titre</label>
                     <input type="text" name="titre" id ="titre" maxlength="80" required '.($a=(isset($_SESSION["article"]["titre"]))?'value="'.$_SESSION["article"]["titre"].'"':"").'>
